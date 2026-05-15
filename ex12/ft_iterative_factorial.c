fca97caa14de3582022231c12a685e1e95ece02a
Jhony Nelson Costa Pinto Da Silva <jhcosta-@c2r9p3.42sp.org.br>
2026-05-15 19:52:27 -0300

    primeiro teste da 42 pra casa

--- ex12/ft_iterative_factorial.c
+++ ex12/ft_iterative_factorial.c
@@ -1,16 +1,50 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_ft.c                                            :+:      :+:    :+:   */
+/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jhcosta- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/05/12 16:45:24 by jhcosta-          #+#    #+#             */
-/*   Updated: 2026/05/12 16:45:26 by jhcosta-         ###   ########.fr       */
+/*   Created: 2026/05/15 16:41:17 by jhcosta-          #+#    #+#             */
+/*   Updated: 2026/05/15 16:41:19 by jhcosta-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_ft(int *nbr)
+#include <unistd.h>
+
+int	ft_iterative_factorial(int nb)
 {
-	*nbr = 42;
+	int	result;
+
+	result = 1;
+	if (nb < 0)
+		return (0);
+	while (nb > 1)
+	{
+		result *= nb;
+		nb--;
+	}
+	return (result);
+}
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	long	nbr;
+
+	nbr = nb;
+	if (nbr >= 10)
+	{
+		ft_putnbr(nbr / 10);
+	}
+	ft_putchar(nbr % 10 + '0');
+}
+
+int	main(void)
+{
+	ft_putnbr(ft_iterative_factorial(3));
 }
---
+++ ex13/ft_recursive_factorial.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jhcosta- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/15 19:17:43 by jhcosta-          #+#    #+#             */
+/*   Updated: 2026/05/15 19:17:45 by jhcosta-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_recursive_factorial(int nb)
+{
+	if (nb < 0)
+		return (0);
+	if (nb > 12)
+		return (0);
+	if (nb == 1)
+		return (1);
+	return (nb * ft_recursive_factorial(nb - 1));
+}
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	long	nbr;
+
+	nbr = nb;
+	if (nbr >= 10)
+	{
+		ft_putnbr(nbr / 10);
+	}
+	ft_putchar(nbr % 10 + '0');
+}
+
+int	main(void)
+{
+	ft_putnbr(ft_recursive_factorial(13));
+}
